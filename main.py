b97636de9ab85d64731e74f510434a979806f242
nmdeni <mekadeni@mail.ru>
2023-02-22 22:15:14 +0300

    начал разработку кнопки и суловий работы

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
 import pandas as pd
 import numpy as np
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import (
+    QApplication, 
+    QMainWindow, 
+    QPushButton, 
+    QLabel, 
+    QWidget, 
+    QVBoxLayout 
+)
+from PyQt6.QtCore import Qt, QSize
 
 def main():
     # Подкласс QMainWindow для настройки главного окна приложения
     class MainWindow(QMainWindow):
         def __init__(self):
-            super().__init__()
+            super(MainWindow, self).__init__()
+
+            num_list = []
+            user_num_list = []
+            user_scores = 0
+
+            #  генерируем числа (боченки)
+            for i in range(0,101):
+                num_list.append(i)
+
+            #  генерируем числа у юзера
+            for i in range(5):
+                user_num_list.append(int(np.random.randint(0, 100)))
 
             # Наполняем окно виджетами
             self.setWindowTitle("RUSSIAN LOTO")
-            button_1 = QPushButton("but  1")
-            button_2 = QPushButton("but 2")
+            scores_label = QLabel(
+                f"Ваши боченки - {' | '.join([str(item) for item in user_num_list])}\nВаши очки - {user_scores}"
+            )
+            
+            button_1 = QPushButton("ДОСТАТЬ БОЧОНОК")
+            
 
-            # Устанавливаем центральный виджет Window.
-            self.setCentralWidget(button_1)
+            # Вставляем остальные 
+            layout = QVBoxLayout()
+            layout.addWidget(scores_label)
+            layout.addWidget(button_1)
 
-    num_list = []
-    user_num_list = []
-    user_scores = 0
+            # Устанавливаем виджеты от центра поумолчанию
+            widget = QWidget()
+            widget.setLayout(layout)
+            self.setFixedSize(QSize(400,300))
+            self.setCentralWidget(widget)
+
     # Приложению нужен один (и только один) экземпляр QApplication.
     # Передаём sys.argv, чтобы разрешить аргументы командной строки для приложения.
     # Если не будете использовать аргументы командной строки, QApplication([]) тоже работает
@@ -28,15 +57,6 @@ def main():
     window = MainWindow()
     # Важно: окно по умолчанию скрыто.
     window.show()
-
-    #  генерируем числа (боченки)
-    for i in range(0,101):
-        num_list.append(i)
-
-    #  генерируем числа у юзера
-    for i in range(5):
-        user_num_list.append(int(np.random.randint(0, 100)))
-    
     # Приложение пойдет дальше, пока вы не выйдете и цикл
     # событий не остановится.
     # Запускаем цикл событий.
